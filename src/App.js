161ad4c0d540041a847fee7f0fd7dbdd260d28e2
Awen233 <dingzhaowen1997@gmail.com>
2018-10-28 17:42:42 -0400

    TopBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { TopBar } from './TopBar'
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-title">Around</div>
-        </header>
+        <TopBar/>
       </div>
     );
   }
